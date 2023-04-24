ee9ae5f6be0d2b0998e6408e1e3c9055f187ed93
Popo <popo@popo.house>
2023-04-24 01:43:36 -0600

    thigh sub update plus license

--- index.py
+++ index.py
@@ -10,9 +10,7 @@ bot = Bot(
     command_prefix=config["prefix"], prefix=config["prefix"],
     owner_ids=config["owners"], command_attrs=dict(hidden=True), help_command=HelpFormat(),
     allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False),
-    intents=discord.Intents(  # kwargs found at https://docs.pycord.dev/en/master/api.html?highlight=discord%20intents#discord.Intents
-        guilds=True, members=True, messages=True, reactions=True, presences=True, message_content=True,
-    )
+    intents=discord.Intents.all()
 )
 
 try:
