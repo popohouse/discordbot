d18118f3391faf6120360abdfaa3129b82661fdc
Popo <popo@popo.house>
2023-04-29 16:39:41 -0600

    Use catapi instead

--- cogs/cat.py
+++ cogs/cat.py
@@ -16,70 +16,30 @@ class CatCog(commands.Cog):
     def cog_unload(self):
         self.daily_cat.cancel()
 
-    @commands.group(invoke_without_command=True)
-    async def cat(self, ctx, *, subcommand=None):
-        """ Posts a random cat, or from specific tag"""
-        if subcommand is None:
-            url = 'https://cataas.com/cat'
-        else:
-            url = f'https://cataas.com/cat/{subcommand}'
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status != 200:
-                    return await ctx.send('Could not get cat image :(')
-                data = await resp.read()
-                content_type = resp.headers['Content-Type']
-                extension = content_type.split('/')[-1]
-                filename = f'cat.{extension}'
-                with open(filename,'wb') as f:
-                    f.write(data)
-        await ctx.send(file=discord.File(filename))
-        os.remove(filename)
 
     @commands.command()
-    async def kitten(self, ctx, ):
-        """ Posts a kitten"""
-        url = 'https://cataas.com/cat/kitten'
+    async def cat(self, ctx):
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status != 200:
-                    return await ctx.send('Could not get cat image :(')
-                data = await resp.read()
-                content_type = resp.headers['Content-Type']
-                extension = content_type.split('/')[-1]
-                filename = f'cat.{extension}'
-                with open(filename,'wb') as f:
-                    f.write(data)
-        await ctx.send(file=discord.File(filename))
-        os.remove(filename)
+            async with session.get('https://api.thecatapi.com/v1/images/search') as response:
+                cat = await response.json()
+                await ctx.send(cat[0]['url'])
 
     @commands.command()
     async def dailycat(self, ctx, channel_id: int):
         self.channel_id = channel_id
-        self.save_data()
         await ctx.send(f"Daily cat posting enabled in channel {channel_id}")
 
     @tasks.loop(minutes=1)
     async def daily_cat(self):
-        """ Set where to post daily cat, posts daily at 12 utc"""
         now = datetime.utcnow()
-        if now.hour == 17 and now.minute == 0:
+        if now.hour == 22 and now.minute == 39:
             if self.channel_id is not None:
                 channel = self.bot.get_channel(self.channel_id)
                 if channel is not None:
-                    url = 'https://cataas.com/cat'
                     async with aiohttp.ClientSession() as session:
-                        async with session.get(url) as resp:
-                            if resp.status != 200:
-                                return await channel.send('Could not get cat image :(')
-                            data = await resp.read()
-                            content_type = resp.headers['Content-Type']
-                            extension = content_type.split('/')[-1]
-                            filename = f'cat.{extension}'
-                            with open(filename,'wb') as f:
-                                f.write(data)
-                    await channel.send("Daily cat posting", file=discord.File(filename))
-                    os.remove(filename)
+                        async with session.get('https://api.thecatapi.com/v1/images/search') as response:
+                            cat = await response.json()
+                            await channel.send(f"Daily cat posting\n{cat[0]['url']}")
 
     def save_data(self):
         with open("cat_data.json", "w") as f:
