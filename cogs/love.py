d544b521085b391112b41df4f6720b6589cdd69c
Popo <popo@popo.house>
2023-05-01 13:54:15 -0600

    More love commands, change some outputs

--- cogs/love.py
+++ cogs/love.py
@@ -19,7 +19,7 @@ class Love(commands.Cog):
                     image_data = await resp.read()
                     image_file = discord.File(BytesIO(image_data), filename=f'kiss{file_ext}')
         if target is None:
-            await ctx.send(f'{ctx.author.mention} kissed themself!', file=image_file)
+            await ctx.send(f'{ctx.author.mention} gets kissed!', file=image_file)
         else:
             await ctx.send(f'{ctx.author.mention} kisses {target.mention}!', file=image_file)
 
@@ -32,9 +32,9 @@ class Love(commands.Cog):
                 file_ext = os.path.splitext(image_url)[1]
                 async with session.get(image_url) as resp:
                     image_data = await resp.read()
-                    image_file = discord.File(BytesIO(image_data), filename=f'kiss{file_ext}')
+                    image_file = discord.File(BytesIO(image_data), filename=f'hug{file_ext}')
         if target is None:
-            await ctx.send(f'{ctx.author.mention} hugs themself!', file=image_file)
+            await ctx.send(f'{ctx.author.mention} gets hugged!', file=image_file)
         else:
             await ctx.send(f'{ctx.author.mention} hugs {target.mention}!', file=image_file)
 
@@ -47,11 +47,86 @@ class Love(commands.Cog):
                 file_ext = os.path.splitext(image_url)[1]
                 async with session.get(image_url) as resp:
                     image_data = await resp.read()
-                    image_file = discord.File(BytesIO(image_data), filename=f'kiss{file_ext}')
+                    image_file = discord.File(BytesIO(image_data), filename=f'cuddle{file_ext}')
         if target is None:
-            await ctx.send(f'{ctx.author.mention} cuddles themself!', file=image_file)
+            await ctx.send(f'{ctx.author.mention} gets cuddled!', file=image_file)
         else:
             await ctx.send(f'{ctx.author.mention} cuddles {target.mention}!', file=image_file)
+
+    @commands.command()
+    async def lick(self, ctx, *, target: discord.Member=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.waifu.pics/sfw/lick') as response:
+                data = await response.json()
+                image_url = data['url']
+                file_ext = os.path.splitext(image_url)[1]
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
+                    image_file = discord.File(BytesIO(image_data), filename=f'lick{file_ext}')
+        if target is None:
+            await ctx.send(f'{ctx.author.mention} gets licked!', file=image_file)
+        else:
+            await ctx.send(f'{ctx.author.mention} licks {target.mention}!', file=image_file)
+
+    @commands.command()
+    async def bonk(self, ctx, *, target: discord.Member=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.waifu.pics/sfw/bonk') as response:
+                data = await response.json()
+                image_url = data['url']
+                file_ext = os.path.splitext(image_url)[1]
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
+                    image_file = discord.File(BytesIO(image_data), filename=f'bonk{file_ext}')
+        if target is None:
+            await ctx.send(f'BONK!', file=image_file)
+        else:
+            await ctx.send(f'BONK! {target.mention}!', file=image_file)            
+            
+    @commands.command()
+    async def slap(self, ctx, *, target: discord.Member=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.waifu.pics/sfw/slap') as response:
+                data = await response.json()
+                image_url = data['url']
+                file_ext = os.path.splitext(image_url)[1]
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
+                    image_file = discord.File(BytesIO(image_data), filename=f'slaps{file_ext}')
+        if target is None:
+            await ctx.send(f'{ctx.author.mention} gets slapped!', file=image_file)
+        else:
+            await ctx.send(f'{ctx.author.mention} slaps {target.mention}!', file=image_file)
+
+    @commands.command()
+    async def wink(self, ctx, *, target: discord.Member=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.waifu.pics/sfw/wink') as response:
+                data = await response.json()
+                image_url = data['url']
+                file_ext = os.path.splitext(image_url)[1]
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
+                    image_file = discord.File(BytesIO(image_data), filename=f'wink{file_ext}')
+        if target is None:
+            await ctx.send(f'{ctx.author.mention} winks!', file=image_file)
+        else:
+            await ctx.send(f'{ctx.author.mention} winks at {target.mention}!', file=image_file)
+
+    @commands.command()
+    async def pat(self, ctx, *, target: discord.Member=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.waifu.pics/sfw/pat') as response:
+                data = await response.json()
+                image_url = data['url']
+                file_ext = os.path.splitext(image_url)[1]
+                async with session.get(image_url) as resp:
+                    image_data = await resp.read()
+                    image_file = discord.File(BytesIO(image_data), filename=f'pat{file_ext}')
+        if target is None:
+            await ctx.send(f'{ctx.author.mention} gets headpats!', file=image_file)
+        else:
+            await ctx.send(f'{ctx.author.mention} gives headpats to {target.mention}!', file=image_file)
 
 async def setup(bot):
     await bot.add_cog(Love(bot))
