a9662277a23f1e2ee6e5204ca9b891330622d717
Popo <popo@popo.house>
2023-05-10 17:08:28 -0600

    Add the descs

--- cogs/animal.py
+++ cogs/animal.py
@@ -10,12 +10,10 @@ class Animal(commands.Cog):
     
     group = app_commands.Group(name="animal", description="Posts animal type you want")
 
-    @app_commands.command(name="top-command")
-    async def my_top_command(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message("Please send non empty command", ephemeral=True)
-    
+
     @group.command(name="dog")
     async def dog(self, interaction: discord.Interaction)-> None:
+        """send dog"""
         async with aiohttp.ClientSession() as session:
             async with session.get('https://random.dog/woof.json') as response:
                 dog = await response.json()
@@ -23,13 +21,16 @@ class Animal(commands.Cog):
 
     @group.command(name="fox")
     async def fox(self, interaction: discord.Interaction)-> None:
+        """Send fox"""
         async with aiohttp.ClientSession() as session:
+
             async with session.get('https://randomfox.ca/floof/') as response:
                 fox = await response.json()
                 await interaction.response.send_message(fox['image'])
 
     @group.command(name="duck")
     async def duck(self, interaction: discord.Interaction)-> None:
+        """Send duck"""
         async with aiohttp.ClientSession() as session:
             async with session.get('https://random-d.uk/api/random') as response:
                 duck = await response.json()
