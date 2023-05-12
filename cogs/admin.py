9111b0fd2a8ba4586ed575d271e6883e253f7c9f
Popo <popo@popo.house>
2023-05-11 23:27:34 -0600

    Undo silly admin.py mistake

--- cogs/admin.py
+++ cogs/admin.py
@@ -4,8 +4,7 @@ from discord import app_commands
 from discord.ext import commands
 import importlib
 import os
-from typing import Literal, Optional
-from discord.ext.commands import Context, Greedy
+
 from discord.ext import commands
 from utils.default import CustomContext
 from utils import permissions, default, http
@@ -15,40 +14,6 @@ from utils.data import DiscordBot
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot: DiscordBot = bot
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.is_owner()
-    async def sync(
-    ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
-        if not guilds:
-            if spec == "~":
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "*":
-                ctx.bot.tree.copy_global_to(guild=ctx.guild)
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "^":
-                ctx.bot.tree.clear_commands(guild=ctx.guild)
-                await ctx.bot.tree.sync(guild=ctx.guild)
-                synced = []
-            else:
-                synced = await ctx.bot.tree.sync()
-
-            await ctx.send(
-                f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
-            )
-            return
-
-        ret = 0
-        for guild in guilds:
-            try:
-                await ctx.bot.tree.sync(guild=guild)
-            except discord.HTTPException:
-                pass
-            else:
-                ret += 1
-
-        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
     @app_commands.command()
     async def amiadmin(self, interaction: discord.Interaction):
