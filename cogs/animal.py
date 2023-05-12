9f8d11a64af67c77040e40b8b484c7c74fc8b1e0
Popo <popo@popo.house>
2023-05-11 23:30:16 -0600

    silly fix cat command

--- cogs/animal.py
+++ cogs/animal.py
@@ -42,7 +42,7 @@ class Animal(commands.Cog):
 
         elif choice.value == ("cat"):
             async with aiohttp.ClientSession() as session:
-                async with session.get('http://127.0.0.1:5888/cat') as response:
+                async with session.get('https://api.popo.house/cat') as response:
                     image_data = await response.read()
                     content_type = response.headers['Content-Type']
                     extension = mimetypes.guess_extension(content_type)
