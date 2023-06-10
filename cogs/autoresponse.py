30571fdc1650bf1cd16a045a82ef5dea68e7cbe7
Popo <popo@popo.house>
2023-06-10 12:49:43 -0600

    Ability to have autoresponse delete user message

--- cogs/autoresponse.py
+++ cogs/autoresponse.py
@@ -49,31 +49,25 @@ class AutoResponseCog(commands.Cog):
                 del self.auto_responses_cache[guild_id]  # Clear existing cache for the guild
             async with self.bot.pool.acquire() as conn:
                 if guild_id is None:
-                    auto_responses = await conn.fetch(
-                        "SELECT guild_id, triggers, response, ping FROM auto_responses"
-                    )
+                    auto_responses = await conn.fetch("SELECT guild_id, triggers, response, ping, deletemsg FROM auto_responses")
                 else:
-                    auto_responses = await conn.fetch(
-                        "SELECT guild_id, triggers, response, ping FROM auto_responses WHERE guild_id = $1",
-                        guild_id
-                    )
+                    auto_responses = await conn.fetch("SELECT guild_id, triggers, response, ping, deletemsg FROM auto_responses WHERE guild_id = $1",guild_id)
                 for row in auto_responses:
                     guild_id = row["guild_id"]
                     triggers = row["triggers"]
                     response = row["response"]
                     ping = row["ping"]
+                    deletemsg = row["deletemsg"]
                     if guild_id not in self.auto_responses_cache:
                         self.auto_responses_cache[guild_id] = []
-                    self.auto_responses_cache[guild_id].append(
-                        {"triggers": triggers, "response": response, "ping": ping}
-                    )
+                    self.auto_responses_cache[guild_id].append({"triggers": triggers, "response": response, "ping": ping, "deletemsg": deletemsg})
         except Exception as e:
             print(f"Failed to fetch auto responses: {str(e)}")
 
     @app_commands.command()
     @commands.guild_only()
     @permissions.has_permissions(manage_guild=True)
-    async def addar(self, interaction: discord.Interaction, trigger: str, response: str, ping: Optional[bool] = False):
+    async def addar(self, interaction: discord.Interaction, trigger: str, response: str, ping: Optional[bool] = False, deletemsg: Optional[bool] = False):
         """Add auto response"""
         if not await permissions.check_priv(self.bot, interaction, None, {"manage_guild": True}):
             return
@@ -95,7 +89,7 @@ class AutoResponseCog(commands.Cog):
             trigger = re.sub(r'^/(.*)/[^/]*$', r'\1', trigger)
             # Store the triggers and response in the database
             async with self.bot.pool.acquire() as conn:
-                await conn.execute("INSERT INTO auto_responses (guild_id, triggers, response, ping) VALUES ($1, $2, $3, $4)", interaction.guild.id, [trigger], json.dumps(response_data), ping)
+                await conn.execute("INSERT INTO auto_responses (guild_id, triggers, response, ping, deletemsg) VALUES ($1, $2, $3, $4, $5)", interaction.guild.id, [trigger], json.dumps(response_data), ping, deletemsg)
                 await self.update_cache(interaction.guild.id)
                 await interaction.response.send_message("Auto response added successfully!", ephemeral=True)
         except Exception as e:
@@ -110,15 +104,10 @@ class AutoResponseCog(commands.Cog):
         try:
             # Check if the guild owns the autoresponse
             async with self.bot.pool.acquire() as conn:
-                auto_response = await conn.fetchrow(
-                    "SELECT guild_id FROM auto_responses WHERE id = $1", id
-                )
+                auto_response = await conn.fetchrow("SELECT guild_id FROM auto_responses WHERE id = $1", id)
                 if auto_response and auto_response["guild_id"] == interaction.guild.id:
                     # Add the alias to the triggers array for the specified auto response
-                    await conn.execute(
-                        "UPDATE auto_responses SET triggers = array_append(triggers, $3) WHERE guild_id = $1 AND id = $2",
-                        interaction.guild.id, id, alias
-                    )
+                    await conn.execute("UPDATE auto_responses SET triggers = array_append(triggers, $3) WHERE guild_id = $1 AND id = $2",interaction.guild.id, id, alias)
                     await self.update_cache(interaction.guild.id)
                     await interaction.response.send_message("Alias added successfully!", ephemeral=True)
                 else:
@@ -136,22 +125,15 @@ class AutoResponseCog(commands.Cog):
             # Check if the guild owns the autoresponse
             async with self.bot.pool.acquire() as conn:
                 auto_response = await conn.fetchrow(
-                    "SELECT guild_id FROM auto_responses WHERE id = $1", id
-                )
+                    "SELECT guild_id FROM auto_responses WHERE id = $1", id)
                 if auto_response and auto_response["guild_id"] == interaction.guild.id:
                     # Remove the alias from the triggers array for the specified auto response
                     if alias is not None:
-                        await conn.execute(
-                            "UPDATE auto_responses SET triggers = array_remove(triggers, $3) WHERE guild_id = $1 AND id = $2",
-                            interaction.guild.id, id, alias
-                        )
+                        await conn.execute("UPDATE auto_responses SET triggers = array_remove(triggers, $3) WHERE guild_id = $1 AND id = $2",interaction.guild.id, id, alias)
                         await self.update_cache(interaction.guild.id)
                         await interaction.response.send_message("Alias removed successfully!", ephemeral=True)
                     else:
-                        await conn.execute(
-                            "DELETE FROM auto_responses WHERE guild_id = $1 AND id = $2",
-                            interaction.guild.id, id
-                        )
+                        await conn.execute("DELETE FROM auto_responses WHERE guild_id = $1 AND id = $2",interaction.guild.id, id)
                         await self.update_cache(interaction.guild.id)
                         await interaction.response.send_message("Response removed successfully!", ephemeral=True)
                 else:
@@ -168,9 +150,7 @@ class AutoResponseCog(commands.Cog):
         try:
             # Retrieve auto responses from the database
             async with self.bot.pool.acquire() as conn:
-                auto_responses = await conn.fetch(
-                    "SELECT id, triggers, response FROM auto_responses WHERE guild_id = $1", interaction.guild.id
-                )
+                auto_responses = await conn.fetch("SELECT id, triggers, response FROM auto_responses WHERE guild_id = $1", interaction.guild.id)
                 # Format the auto responses as a list of strings (pages)
                 pages = []
                 page_limit = 2000  # Character limit per page
@@ -183,9 +163,7 @@ class AutoResponseCog(commands.Cog):
                     response_str += entry
                 pages.append(response_str)
             view = Buttons(pages)
-            view.page_counter = await interaction.response.send_message(
-                f"{pages[0]}Page 1/{len(pages)}", view=view, ephemeral=True
-            )
+            view.page_counter = await interaction.response.send_message(f"{pages[0]}Page 1/{len(pages)}", view=view, ephemeral=True)
         except Exception as e:
             await interaction.response.send_message(f"Failed to list auto responses: {str(e)}", ephemeral=True)
 
@@ -200,31 +178,39 @@ class AutoResponseCog(commands.Cog):
                         if re.search(trigger, message.content):
                             response = response_data["response"]
                             ping = response_data["ping"]
+                            deletemsg = response_data["deletemsg"]
                             try:
                                 response_data = json.loads(response)
                                 if isinstance(response_data, dict) and "embeds" in response_data:
                                     for embed in response_data["embeds"]:
                                         if ping:
-                                            await message.channel.send(
-                                                f"{message.author.mention}",
-                                                embed=discord.Embed.from_dict(embed),
-                                            )
+                                            if deletemsg:
+                                                await message.delete()
+                                                await message.channel.send(f"{message.author.mention}",embed=discord.Embed.from_dict(embed),)
+                                            else:
+                                                await message.channel.send(f"{message.author.mention}",embed=discord.Embed.from_dict(embed),)
                                         else:
-                                            await message.channel.send(
-                                                embed=discord.Embed.from_dict(embed)
-                                            )
+                                            if deletemsg:
+                                                await message.delete()
+                                                await message.channel.send(embed=discord.Embed.from_dict(embed))
+                                            else: 
+                                                await message.channel.send(embed=discord.Embed.from_dict(embed))
                                 else:
                                     raise json.JSONDecodeError("", "", 0)
                             except json.JSONDecodeError:
-                                escaped_response = response.replace("{", "{{").replace(
-                                    "}", "}}"
-                                ).strip('"')
+                                escaped_response = response.replace("{", "{{").replace("}", "}}").strip('"')
                                 if ping:
-                                    await message.channel.send(
-                                        f"{message.author.mention} {escaped_response}"
-                                    )
+                                    if deletemsg:
+                                        await message.delete()
+                                        await message.channel.send(f"{message.author.mention} {escaped_response}")
+                                    else:
+                                        await message.channel.send(f"{message.author.mention} {escaped_response}")
                                 else:
-                                    await message.channel.send(escaped_response)
+                                    if deletemsg:
+                                        await message.delete()
+                                        await message.channel.send(escaped_response)
+                                    else: 
+                                        await message.channel.send(escaped_response)
                             break
 
 
--- utils/database.py
+++ utils/database.py
@@ -4,73 +4,97 @@ config = Config.from_env()
 
 async def create_tables(bot):
     async with bot.pool.acquire() as conn:
-        await conn.execute('''
-            CREATE TABLE IF NOT EXISTS logging (
-                guild_id BIGINT PRIMARY KEY,
-                channel_id BIGINT,
-                log_deleted_messages BOOLEAN,
-                log_edited_messages BOOLEAN,
-                log_nickname_changes BOOLEAN,
-                log_member_join_leave BOOLEAN,
-                log_member_kick BOOLEAN,
-                log_member_ban_unban BOOLEAN,
-                modlog_id BIGINT
+        # Define current version of schema here
+        expected_version = 1
+        schema_version_exists = await conn.fetchval('''
+            SELECT EXISTS (
+                SELECT 1
+                FROM information_schema.tables
+                WHERE table_name = 'schema_version'
             )
         ''')
-        await conn.execute('''
-            CREATE TABLE IF NOT EXISTS dailycat (
+        if not schema_version_exists:
+            await conn.execute('''
+                CREATE TABLE IF NOT EXISTS logging (
+                    guild_id BIGINT PRIMARY KEY,
+                    channel_id BIGINT,
+                    log_deleted_messages BOOLEAN,
+                    log_edited_messages BOOLEAN,
+                    log_nickname_changes BOOLEAN,
+                    log_member_join_leave BOOLEAN,
+                    log_member_kick BOOLEAN,
+                    log_member_ban_unban BOOLEAN,
+                    modlog_id BIGINT
+                )
+            ''')
+            await conn.execute('''
+                CREATE TABLE IF NOT EXISTS dailycat (
+                    guild_id BIGINT PRIMARY KEY,
+                    channel_id BIGINT,
+                    post_time TEXT
+                )
+            ''')
+            await conn.execute('''
+                CREATE TABLE IF NOT EXISTS reaction_roles (
+                    id SERIAL PRIMARY KEY,
+                    guild_id BIGINT NOT NULL,
+                    message_id BIGINT NOT NULL,
+                    emoji TEXT NOT NULL,
+                    role_id BIGINT NOT NULL,
+                    UNIQUE (guild_id, message_id, emoji)
+            );
+            ''')
+            await conn.execute(''' 
+                CREATE TABLE IF NOT EXISTS timezones (
+                    user_id BIGINT PRIMARY KEY,
+                    timezone TEXT NOT NULL
+                )
+            ''')
+            await conn.execute(''' 
+                CREATE TABLE IF NOT EXISTS mod_role_id (
+                    guild_id BIGINT PRIMARY KEY,
+                    role_id BIGINT NOT NULL
+                    )
+                ''')
+            await conn.execute('''
+                CREATE TABLE IF NOT EXISTS birthdays (
+                    guild_id BIGINT NOT NULL,
+                    user_id BIGINT NOT NULL,
+                    date DATE NOT NULL,
+                    PRIMARY KEY (guild_id, user_id)
+                    )
+                ''')
+            await conn.execute ('''
+            CREATE TABLE IF NOT EXISTS birthday_extras(
                 guild_id BIGINT PRIMARY KEY,
                 channel_id BIGINT,
-                post_time TEXT
-            )
-        ''')
-        await conn.execute('''
-            CREATE TABLE IF NOT EXISTS reaction_roles (
+                role_id BIGINT
+                    )
+                ''')
+            await conn.execute('''
+                CREATE TABLE IF NOT EXISTS auto_responses(
                 id SERIAL PRIMARY KEY,
                 guild_id BIGINT NOT NULL,
-                message_id BIGINT NOT NULL,
-                emoji TEXT NOT NULL,
-                role_id BIGINT NOT NULL,
-                UNIQUE (guild_id, message_id, emoji)
-        );
-        ''')
-        await conn.execute(''' 
-            CREATE TABLE IF NOT EXISTS timezones (
-                user_id BIGINT PRIMARY KEY,
-                timezone TEXT NOT NULL
-            )
-        ''')
-        await conn.execute(''' 
-            CREATE TABLE IF NOT EXISTS mod_role_id (
-                guild_id BIGINT PRIMARY KEY,
-                role_id BIGINT NOT NULL
-                )
-            ''')
-        await conn.execute('''
-            CREATE TABLE IF NOT EXISTS birthdays (
-                guild_id BIGINT NOT NULL,
-                user_id BIGINT NOT NULL,
-                date DATE NOT NULL,
-                PRIMARY KEY (guild_id, user_id)
-                )
-            ''')
-        await conn.execute ('''
-        CREATE TABLE IF NOT EXISTS birthday_extras(
-            guild_id BIGINT PRIMARY KEY,
-            channel_id BIGINT,
-            role_id BIGINT
-                )
-            ''')
-        await conn.execute('''
-            CREATE TABLE IF NOT EXISTS auto_responses(
-            id SERIAL PRIMARY KEY,
-            guild_id BIGINT NOT NULL,
-            triggers TEXT[],
-            response TEXT,
-            ping BOOLEAN,
-            UNIQUE (guild_id, triggers, response)
-                ) 
-            ''')
+                triggers TEXT[],
+                response TEXT,
+                ping BOOLEAN,
+                deletemsg BOOLEAN,
+                UNIQUE (guild_id, triggers, response)
+                    ) 
+                ''')
+            await conn.execute('''CREATE TABLE schema_version (version INT NOT NULL)''')
+            await conn.execute('INSERT INTO schema_version (version) VALUES ($1)', expected_version)
+        else:
+            stored_version = await conn.fetchval('SELECT version FROM schema_version')
+            if stored_version == expected_version:
+                return
+            elif stored_version < expected_version:
+                # Please actually add here, and remove the return statement simply here as place holder currently
+                #Also have it work through migrations in case of multiple schema updates
+                return
+            else:
+                #Hey future me please add better error handling in case of users having issues with schema updates.
+                raise ValueError('Invalid schema version detected')
 
 async def populate_tables(bot):
    async with bot.pool.acquire() as conn:
