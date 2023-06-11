2b4792c3d88e952b11736abcb8e9b7ed53b605d6
Popo <admin@popo.house>
2023-06-11 13:31:04 -0600

    indentation

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -22,26 +22,26 @@ class SassyCog(commands.Cog):
         app_commands.Choice(name="UwUbot", value="uwuify"),
     ])
     async def gpt(self, interaction: discord.Interaction, prompt: str, model: Optional[app_commands.Choice[str]]):
-            try:
-                model_name = model.name if model else 'ChatGPT'
-                print (interaction.user.name, prompt, model_name)
-                models = {
-                'Claude': 'a2',
-                'ChatGPT': 'chinchilla',
-                'UwUbot': 'uwuify'
-                        }
-                base = f'*model*: `{model_name}`\n'
-                token = config.poe_token
-                client = poe.Client(token)
-                await interaction.response.send_message(base)
-                base += '\n'
-                completion = client.send_message(models[model_name], prompt, with_chat_break=True)
-                for token in completion:
-                    base += token['text_new']
-                    base = base.replace('Discord Message:', '')
-                    await interaction.edit_original_response(content=base)
-            except Exception as e:
-                print (e)
+        try:
+            model_name = model.name if model else 'ChatGPT'
+            print (interaction.user.name, prompt, model_name)
+            models = {
+            'Claude': 'a2',
+            'ChatGPT': 'chinchilla',
+            'UwUbot': 'uwuify'
+            }
+            base = f'*model*: `{model_name}`\n'
+            token = config.poe_token
+            client = poe.Client(token)
+            await interaction.response.send_message(base)
+            base += '\n'
+            completion = client.send_message(models[model_name], prompt, with_chat_break=True)
+            for token in completion:
+                base += token['text_new']
+                base = base.replace('Discord Message:', '')
+                await interaction.edit_original_response(content=base)
+        except Exception as e:
+            print (e)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
