322efdadbdca5c8f7ca2f2db122d932d524abe14
Popo <admin@popo.house>
2023-06-11 13:34:30 -0600

    verified commits lesgo

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -24,11 +24,10 @@ class SassyCog(commands.Cog):
     async def gpt(self, interaction: discord.Interaction, prompt: str, model: Optional[app_commands.Choice[str]]):
         try:
             model_name = model.name if model else 'ChatGPT'
-            print (interaction.user.name, prompt, model_name)
             models = {
-            'Claude': 'a2',
-            'ChatGPT': 'chinchilla',
-            'UwUbot': 'uwuify'
+                'Claude': 'a2',
+                'ChatGPT': 'chinchilla',
+                'UwUbot': 'uwuify'
             }
             base = f'*model*: `{model_name}`\n'
             token = config.poe_token
@@ -41,7 +40,7 @@ class SassyCog(commands.Cog):
                 base = base.replace('Discord Message:', '')
                 await interaction.edit_original_response(content=base)
         except Exception as e:
-            print (e)
+            print(e)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
